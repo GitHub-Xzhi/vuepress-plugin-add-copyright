a3693f221c327bc2bca1ae8dc2f69ccc2e8eabd5
Xzhi <cx_zhi@qq.com>
2021-05-03 10:31:20 +0800

    【修复】修复编译的时候报错的异常

--- enhanceAppFile.js
+++ enhanceAppFile.js
@@ -1,41 +1,51 @@
 const rootMixin = require("./rootMixin.js");
 
 export default () => {
-  function addCopy(e) {
-    if (disabled) {
-      return;
-    }
-    let copyTxt = "";
-    // 取消默认的复制事件
-    e.preventDefault();
-    const enable = rootMixin.copyrightEnable;
-    copyTxt = window.getSelection(0).toString();
-    const copyTxtLength = copyTxt.length;
-    const thanMinLength = copyTxtLength > minLength;
-    const webLink = `${window.location.href}`;
-    let copyrightContent = `${authorNameTag}${authorName}\n${webLinkTag}${webLink}\n${statement}`;
-    if (allCustom !=='') {
-      copyrightContent = allCustom.replace(/#{webLink}/g,`${webLink}`)
-    }
-    if (thanMinLength && (enable === undefined || enable)) {
-      if (copyrightLoca === 'before') {
-        copyTxt = `${copyrightContent}\n${copyTxt}`;
-      } else {
-        copyTxt = `${copyTxt}\n${copyrightContent}`;
-      }
-    }
-    const clipboardData = e.clipboardData || window.clipboardData;
-    if (thanMinLength && noCopy) {
-      e.preventDefault();
-    } else {
-      clipboardData.setData('text', copyTxt);
-    }
-  }
 
-  document.addEventListener("cut", e => {
-    addCopy(e)
-  });
-  document.addEventListener("copy", e => {
-    addCopy(e)
-  });
+  setTimeout(() => {
+    try {
+      // 对document的判断是防止编译的时候报错
+      document && (() => {
+        function addCopy(e) {
+          if (disabled) {
+            return;
+          }
+          let copyTxt = "";
+          // 取消默认的复制事件
+          e.preventDefault();
+          const enable = rootMixin.copyrightEnable;
+          copyTxt = window.getSelection(0).toString();
+          const copyTxtLength = copyTxt.length;
+          const thanMinLength = copyTxtLength > minLength;
+          const webLink = `${window.location.href}`;
+          let copyrightContent = `${authorNameTag}${authorName}\n${webLinkTag}${webLink}\n${statement}`;
+          if (allCustom !== '') {
+            copyrightContent = allCustom.replace(/#{webLink}/g, `${webLink}`)
+          }
+          if (thanMinLength && (enable === undefined || enable)) {
+            if (copyrightLoca === 'before') {
+              copyTxt = `${copyrightContent}\n${copyTxt}`;
+            } else {
+              copyTxt = `${copyTxt}\n${copyrightContent}`;
+            }
+          }
+          const clipboardData = e.clipboardData || window.clipboardData;
+          if (thanMinLength && noCopy) {
+            e.preventDefault();
+          } else {
+            clipboardData.setData('text', copyTxt);
+          }
+        }
+
+        document.addEventListener("cut", e => {
+          addCopy(e)
+        });
+        document.addEventListener("copy", e => {
+          addCopy(e)
+        });
+      })()
+    } catch (e) {
+      // console.error(e.message)
+    }
+  }, 500)
 }
